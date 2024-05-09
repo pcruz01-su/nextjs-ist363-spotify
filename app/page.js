942cc3f6d0d2b24e44d82796f80fd2d0cdc62874
pcruz01-su <pcruz01@syr.edu>
2024-05-09 12:57:03 -0400

    part 1

--- app/page.js
+++ app/page.js
@@ -1,5 +1,29 @@
-const Homepage = () => {
-  return <div>
-    <h1>Homepage</h1>
-  </div>
+"use client";
+
+import { useEffect, useState } from "react";
+
+function HomePage() {
+  const [artist, setArtist] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch("/api/artists")
+        .then((res) => res.json())
+        .then((data) => {
+          setArtist(data);
+          setIsLoading(false);
+        });
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading artists</p>;
+
+  return <div>{artist.name}</div>;
 }
+
+export default HomePage;
